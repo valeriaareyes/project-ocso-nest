5c0a747cf84bed55115309d94dff43e5bd0a8350
Laura Valeria Velazquez Reyes <lvelazquez30@alumnos.uaq.mx>
2026-09-24 22:53:21 -0600

    Upload photo in employee resource

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
@@ -10,6 +11,15 @@ export class EmployeesController {
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
+  }
+
+  @Post('upload')
+ @UseInterceptors(FileInterceptor('file', {
+  dest: "./src/employees/employees-photos"
+ }))
+ uploadPhoto(@UploadedFile() file: Express.Multer.File){
+  console.log(file);
+    return "OK"
   }
 
   @Get()
