d55fd587e7841a5a06ac6a8c04ce47a8fe88e3bb
Laura Valeria Velazquez Reyes <lvelazquez30@alumnos.uaq.mx>
2026-09-24 22:00:46 -0600

    Better get methosd

--- src/providers/providers.service.ts
+++ src/providers/providers.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
+import { Provider } from './entities/provider.entity';
+import { Repository, Like } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProvidersService {
+  constructor(
+    @InjectRepository(Provider)
+    private providerRepository: Repository<Provider>
+  ){}
   create(createProviderDto: CreateProviderDto) {
-    return 'This action adds a new provider';
+    return this.providerRepository.save(createProviderDto)
   }
-
   findAll() {
-    return `This action returns all providers`;
-  }
+  return this.providerRepository.find();
+}
 
-  findOne(id: number) {
-    return `This action returns a #${id} provider`;
-  }
+  findOne(id: string) {
+  return this.providerRepository.findOneBy({
+    providerId: id
+  })
+}
 
-  update(id: number, updateProviderDto: UpdateProviderDto) {
-    return `This action updates a #${id} provider`;
-  }
+findOneByName(name: string){
+  const provider = this.providerRepository.findBy({
+    providerName: Like(`%${name}$`)
+  })
+  if (!provider) throw new NotFoundException()
+    return provider;
+}
 
-  remove(id: number) {
-    return `This action removes a #${id} provider`;
-  }
+async update(id: string, updateProviderDto: UpdateProviderDto) {
+  const product = await this.providerRepository.preload({
+    providerId: id,
+    ...updateProviderDto
+  })
+
+  return this.providerRepository.save(product);
+}
+
+remove(id: string) {
+  this.providerRepository.delete({
+    providerId: id
+  })
+}
 }
